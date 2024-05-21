70c9ccc8684a02ca4e6470b373fdd10bb9490153
AidenK-GH <81008205+AidenK-GH@users.noreply.github.com>
2024-05-21 15:53:50 +0300

    change AidenKaminsky massage

--- AidenKaminsky.java
+++ AidenKaminsky.java
@@ -1,7 +1,7 @@
 public class AidenKaminsky{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Aiden Kaminsky ..."+i);
+			System.out.println("Aiden gets 100 in Devops ..."+i);
 		}
 	}
 }
